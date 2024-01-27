21509033305259a2fb9bcd3123d945c4ac1424c8
alexphee <alexfilippas@gmail.com>
2024-01-27 17:06:14 +0200

    added spinning thingy

--- js/scripts.js
+++ js/scripts.js
@@ -37,3 +37,18 @@
     });
 })(jQuery);
 
+$(function() {
+    var rotation = 0, 
+        scrollLoc = $(document).scrollTop();
+    $(window).scroll(function() {
+        var newLoc = $(document).scrollTop();
+        var diff = scrollLoc - newLoc;
+        rotation += diff, scrollLoc = newLoc;
+        var rotationStr = "rotate(" + rotation + "deg)";
+        $(".gear").css({
+            "-webkit-transform": rotationStr,
+            "-moz-transform": rotationStr,
+            "transform": rotationStr
+        });
+    });
+})
